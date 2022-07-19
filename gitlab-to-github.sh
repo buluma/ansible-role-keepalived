33d8a162eb011dfd8dbba3e558d39479d74946ec
Michael Buluma <buluma@users.noreply.github.com>
2022-07-19 19:57:55 +0300

    Local push

---
+++ gitlab-to-github.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Arguments:
+#   ${1}: Username
+#   ${2}: Repository
+
+# Clone the repo from GitLab using the `--mirror` option.
+# git clone --mirror "git@gitlab.com:${1}/${2}.git"
+git add . && git commit -m "Local push"
+
+# Push to GitHub using the `--mirror` option. The `--no-verify` option skips any hooks.
+# git -C "${2}" push --no-verify --mirror "git@github.com:${1}/${2}.git"
+# git push --no-verify --mirror git@gitlab.com:buluma/ansible-role-keepalived.git
+# git push origin-new
+
+# Set push URL to the mirror location.
+# git -C "${2}" remote set-url --push origin "git@github.com:${1}/${2}.git"
+git remote set-url --push origin-new git@gitlab.com:buluma/ansible-role-keepalived.git
+
+# To periodically update the repo on GitHub with what you have in GitLab.
+# git -C "${2}" fetch -p origin
+# git -C "${2}" push --no-verify --mirror
